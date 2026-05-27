88f1464b1ed0dfaf32f506ab3c3a87f8152c158f
IlyRac <iliass.rached@gmail.com>
2026-05-27 14:01:05 +0000

    Upload The Mod Template

---
+++ src/client/java/com/ilyrac/truesnow/client/TrueSnowClient.java
@@ -0,0 +1,10 @@
+package com.ilyrac.truesnow.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TrueSnowClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
